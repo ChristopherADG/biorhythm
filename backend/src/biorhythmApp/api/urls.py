dfd5ce5e03f095e9972415145f6a4e4a56a007e9
Klaus Kientzle <klauskie19@gmail.com>
2020-03-26 21:07:44 -0700

    [BIO-2] loggin and loggout backend implementation

--- backend/src/biorhythmApp/api/urls.py
+++ backend/src/biorhythmApp/api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import UserListView, UserDetailView, UserCreateView
+from .sessions import LoginView, LogoutView
 
 urlpatterns = [
     path('users/', UserListView.as_view()),
     path('users/<pk>', UserDetailView.as_view()),
     path('users/create/', UserCreateView.as_view()),
     path('users/<pk>/update/', UserCreateView.as_view()),
+    path('users/login/', LoginView.as_view()),
+    path('users/logout/', LogoutView.as_view()),
 ]
