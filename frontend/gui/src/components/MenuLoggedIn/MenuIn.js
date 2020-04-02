60517955a87c0d36ddbf87d37a5c959971d26328
Klaus Kientzle <klauskt@hotmail.com>
2020-04-02 12:40:27 -0700

    [BIO-9] menus for log in and out

---
+++ frontend/gui/src/components/MenuLoggedIn/MenuIn.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const MenuIn = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+                <span className="navbar-brand">Biorhythm</span>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <Link to='/dashboard' className="nav-link">Dashboard</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/events' className="nav-link">Events</Link>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Account
+                            </a>
+                            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <Link to='/profile' className="dropdown-item">Profile</Link>
+                                <Link to='/logout' className="dropdown-item">Log out</Link>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default MenuIn;
--- frontend/gui/src/containers/App/App.js
+++ frontend/gui/src/containers/App/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from '../../components/Routes/Routes';
+import MenuOut from '../../components/MenuLoggedOut/MenuOut';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <MenuOut />
         <Routes />
       </BrowserRouter>
     </div>
