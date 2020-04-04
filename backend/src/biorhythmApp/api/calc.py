13a09587a51fe7a65aaf33d8646380af1e63e333
Klaus Kientzle <klauskt@hotmail.com>
2020-04-04 14:33:39 -0700

    [BIO-11] HTTP GET added

--- backend/src/biorhythmApp/api/calc.py
+++ backend/src/biorhythmApp/api/calc.py
@@ -1,18 +1,65 @@
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import permissions, status
 from datetime import datetime, timedelta
 import math
+from .sessions import get_logged_user
+
+
+class BioView(APIView):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = (permissions.AllowAny,)
+
+    def get(self, request):
+
+        user = get_logged_user(request)
+        if not user:
+            return Response({'error': "Can't find user or you are not logged in"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        limit = 15
+        scope = None
+        target_date = datetime.now()
+
+        if request.GET.get('target_date'):
+            target_date = datetime.strptime(request.GET.get('target_date'), '%Y-%m-%d')
+
+        if request.GET.get('range'):
+            scope = int(request.GET.get('range'))
+
+        if request.GET.get('limit'):
+            limit = int(request.GET.get('limit'))
+
+        birthday_dt = datetime.combine(user.birthdate, datetime.min.time())
+        if scope:
+            return Response(
+                ListBio(birthday_dt, scope)
+                .get_next_days(target_date, limit)
+                .to_dic(),
+                status=status.HTTP_200_OK
+            )
+        else:
+            return Response(
+                Biorhythm(birthday_dt)
+                .calculate(target_date, limit)
+                .to_dic(),
+                status=status.HTTP_200_OK
+            )
 
 
 class Biorhythm:
 
-    def __init__(self, bdate):
-        self.bdate = bdate
+    def __init__(self, init_date):
+        self.init_date = init_date
         self.physical = 0
         self.emotional = 0
         self.intellectual = 0
         self.days_since = 0
+        self.target_date = 0
 
     def get_time_difference(self, target_date):
-        return (target_date - self.bdate).days
+        self.target_date = target_date
+        return (target_date - self.init_date).days
 
     def calculate(self, target_date=datetime.now(), limit=15):
         self.days_since = self.get_time_difference(target_date)
@@ -25,19 +72,36 @@ class Biorhythm:
     def to_string(self):
         return "p: {}, e: {}, i: {}".format(self.physical, self.emotional, self.intellectual)
 
+    def to_dic(self):
+        return {
+            "phy": self.physical,
+            "emo": self.emotional,
+            "int": self.intellectual,
+            "days_since": self.days_since,
+            "init_date": int(self.init_date.timestamp() * 1000),
+            "init_date_str": self.init_date.strftime('%Y/%m/%d'),
+            "target_date": int(self.target_date.timestamp() * 1000),
+            "target_date_str": self.target_date.strftime('%Y/%m/%d')
+        }
+
 
 class ListBio:
 
-    def __init__(self, bdate, range):
-        self.bdate = bdate
-        self.range = range + 1
+    def __init__(self, birth_date, scope):
+        self.birth_date = birth_date
+        self.scope = scope + 1
         self.list = []
 
-    def get_next_days(self, limit=15):
-        today = datetime.now()
+    def get_next_days(self, init_date=datetime.now(), limit=15):
 
-        for i in range(self.range):
-            target = today + timedelta(days=i)
-            self.list.append(Biorhythm(self.bdate).calculate(target_date=target, limit=limit))
+        for i in range(self.scope):
+            target_date = init_date + timedelta(days=i)
+            self.list.append(Biorhythm(self.birth_date).calculate(target_date=target_date, limit=limit))
 
-        return self.list
+        return self
+
+    def to_dic(self):
+        structure = []
+        for i, bio in enumerate(self.list):
+            structure.append(bio.to_dic())
+        return structure
